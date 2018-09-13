2e5cc7dd71dd6a794fef80f0d33d9d4d23a52e39
ghspbravo <gmspbravo@gmail.com>
2018-09-13 10:59:35 +0500

    add ScrollMagic fix

--- src/animations.js
+++ src/animations.js
@@ -1,4 +1,7 @@
 import { TimelineMax, TweenMax, CSSPlugin, AttrPlugin }  from "gsap/all";
+import ScrollMagic from 'ScrollMagic';
+import 'animation.gsap';
+import 'debug.addIndicators';
 
 //without this line, CSSPlugin and AttrPlugin may get dropped by your bundler...
 const plugins = [ CSSPlugin, AttrPlugin ];
