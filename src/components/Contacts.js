a5b3ae57e9236791606a741b5e2e535d9cfa062d
ghspbravo <gmspbravo@gmail.com>
2018-09-24 14:32:12 +0500

    adress change

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -15,7 +15,7 @@ export default class Contacts extends Component {
             <section id='contacts'>
                 <div style={{textAlign: 'center', zIndex: 5, position: 'relative'}}>
                     <h1 style={{marginTop: getVH(190), marginBottom: '10vh', fontSize: window.innerWidth < 992 ? window.innerWidth < 568 ? window.innerWidth < 400 ? '1.5rem' : '2rem' : '3rem' : null}}>Начнем опережать <br/> ваших конкурентов?</h1>
-                    <a style={{textDecoration: 'none', color: 'black', fontSize: window.innerWidth < 992 ? window.innerWidth < 568 ? '1.8rem' : '2rem' : '3.75rem'}} href="mailto:welcome@zotov.ru">welcome@zotov.ru</a>
+                    <a style={{textDecoration: 'none', color: 'black', fontSize: window.innerWidth < 992 ? window.innerWidth < 568 ? '1.8rem' : '2rem' : '3.75rem'}} href="mailto:info@zotov.one">info@zotov.one</a>
                     <p style={{marginTop: '25vh'}}>Екатеринбург, Ленина 66</p>
                     <a href="https://telegram.me/badoptimist"><img style={{marginTop: '2vh', userSelect: 'none'}} src={telegram} alt="telegram"/></a>
                 </div>
