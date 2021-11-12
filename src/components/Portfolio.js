c12963cbaf9b3650a6863c2799efbb6efb66fd64
ghspbravo <gmspbravo@gmail.com>
2021-11-12 20:02:48 +0500

    some edits ?

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -88,65 +88,15 @@ class Portfolio extends Component {
       ? (
         <section id='portfolio'>
           <h1 style={{ marginBottom: getVH(50) }}>{this.props.t("portfolio.title")}</h1>
-          {window.innerWidth > 992
-            ? <div style={{ display: 'flex' }}>
-              <div style={{ padding: '0 5px', flex: '1 1 25%' }}>
-                <ul onMouseOver={() => this.setState({ hovered: true })} onMouseLeave={() => this.setState({ hovered: false })}>
-                  {this.projects.map((project, index) => <li key={index} style={{ margin: '10px', padding: '5px', backgroundColor: index === this.state.current && !this.state.hovered ? '#363BAE' : null, cursor: 'pointer', color: index === this.state.current && !this.state.hovered ? 'white' : 'black' }}
-                    onClick={() => this.setState({ current: index })}><p>{project.title}</p></li>)}
-                </ul>
-              </div>
-              <div style={{ flex: '1 1 50%' }}>
-                <img ref={this.desktopImageRef} data-src={this.projects[this.state.current].thumbnail} alt="project" style={{ height: getVH(350), width: '100%', objectFit: 'cover' }} />
-                <p style={{ color: '#A59292', width: '50%', marginLeft: 'auto' }}>
-                  {this.projects[this.state.current].tags}
-                </p>
-              </div>
-              <div style={{ flex: '1 1 25%' }}>
-                <p style={{ height: getVH(350), paddingLeft: '5px', overflowY: 'auto' }}>
-                  {this.projects[this.state.current].task}
-                </p>
-                <a id='project_link_button' target='blank' href={this.projects[this.state.current].url} style={{ backgroundColor: '#363BAE', transition: 'background-color 0.3s', color: 'white', padding: '25px', textDecoration: 'none', display: 'block', width: 'fit-content' }}>{this.projects[this.state.current].url === null ? this.props.t("portfolio.buttons.soon") : this.props.t("portfolio.buttons.open")}</a>
-              </div>
-            </div>
-            : window.innerWidth > 568
-              ? <div style={{ display: 'flex' }}>
-                <div style={{ padding: '0 5px', flex: '1 1 25%' }}>
-                  <ul>
-                    {this.projects.map((project, index) => <li key={index} style={{ margin: '10px', marginLeft: '-30px', padding: '5px', backgroundColor: index === this.state.current ? '#363BAE' : null, cursor: 'pointer', color: index === this.state.current ? 'white' : 'black' }}
-                      onClick={() => this.setState({ current: index })}><p>{project.title}</p></li>)}
-                  </ul>
-                </div>
-                <div style={{ padding: '0 5px', flex: '1 1 75%', display: 'flex', flexWrap: 'wrap' }}>
-                  <img src={this.projects[this.state.current].thumbnail} alt="project" style={{ marginBottom: '5vh', height: getVH(350), width: '100%', objectFit: 'cover' }} />
-                  <p style={{ padding: '0 5px', flex: '2 1 100%' }}>
-                    {this.projects[this.state.current].task}
-                  </p>
-                  <p style={{ color: '#A59292', padding: '0 5px', flex: '1 2 100%', marginTop: '2vh' }}>
-                    {this.projects[this.state.current].tags}
-                  </p>
-                  <a id='project_link_button' target='blank' href={this.projects[this.state.current].url} style={{ marginTop: '5vh', backgroundColor: '#363BAE', transition: 'background-color 0.3s', color: 'white', padding: '25px', textDecoration: 'none', display: 'block', width: 'fit-content' }}>{this.projects[this.state.current].url === null ? this.props.t("portfolio.buttons.soon") : 'Посмотреть'}</a>
-                </div>
-              </div>
-              : <div style={{ display: 'flex', flexDirection: 'column', }}>
-                <div style={{ padding: '0 5px', flex: '1 1 100%' }}>
-                  <ul>
-                    {this.projects.map((project, index) => <li key={index} style={{ margin: '10px', padding: '5px', backgroundColor: index === this.state.current ? '#363BAE' : null, cursor: 'pointer', color: index === this.state.current ? 'white' : 'black' }}
-                      onClick={() => this.setState({ current: index })}><p>{project.title}</p></li>)}
-                  </ul>
-                </div>
-                <div style={{ padding: '0 5px', flex: '1 1 100%', display: 'flex', flexWrap: 'wrap', marginTop: '5vh' }}>
-                  <img ref={this.mobileImageRef} data-src={this.projects[this.state.current].thumbnail} alt="project" style={{ marginBottom: '5vh', height: getVH(350), width: '100%', objectFit: 'cover' }} />
-                  <p style={{ padding: '0 5px', flex: '2 1 100%' }}>
-                    {this.projects[this.state.current].task}
-                  </p>
-                  <p style={{ color: '#A59292', padding: '0 5px', flex: '1 2 100%', marginTop: '1vh' }}>
-                    {this.projects[this.state.current].tags}
-                  </p>
-                  <a id='project_link_button' target='blank' href={this.projects[this.state.current].url} style={{ marginTop: '5vh', backgroundColor: '#363BAE', transition: 'background-color 0.3s', color: 'white', padding: '25px', textDecoration: 'none', display: 'block', width: 'fit-content' }}>{this.projects[this.state.current].url === null ? this.props.t("portfolio.buttons.soon") : 'Посмотреть'}</a>
-                </div>
-              </div>
-          }
+          <div style={{ display: 'flex', flexWrap: 'wrap' }} >
+            {this.projects.map((project, index) => <div className='portfolio-item' key={index} style={{ width: '50%', padding: '5px' }}
+              onClick={() => this.setState({ current: index })}>
+              <a style={{ textDecoration: 'none' }} target='_blank' href={this.projects[this.state.current].url} >
+                <p className='portfolio-item__title'>{project.title}</p>
+                <p style={{ marginTop: '20px', fontSize: '14px', color: '#A59292' }}>{this.projects[this.state.current].tags}</p>
+              </a>
+            </div>)}
+          </div>
         </section>
       )
       : (
